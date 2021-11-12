9ba5d23c6be6bbdc4e70d8f6fd6ca6e0e245915e
YourPalTaika <94190326+YourPalTaika@users.noreply.github.com>
2021-11-12 20:23:46 +0000

    Update debinux-deploy.sh

--- debinux-deploy.sh
+++ debinux-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install i3 lightdm lightdm-gtk-greeter pcmanfm inkscape audacity gimp neofetch htop firefox-esr feh terminator pulseaudio pavucontrol geany vim
+sudo apt-get install i3 lightdm lightdm-gtk-greeter pcmanfm inkscape audacity gimp neofetch htop firefox-esr feh terminator pulseaudio pavucontrol geany vim lxappearance
 mkdir ~/Pictures
 mkdir ~/.config/i3/
 mkdir ~/.config/gtk-3.0/
