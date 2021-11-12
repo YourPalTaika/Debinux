0f56f3f3c9d378af8a1a60f4c3db9783422275af
YourPalTaika <94190326+YourPalTaika@users.noreply.github.com>
2021-11-12 17:27:27 +0000

    Update debinux-deploy.sh

--- debinux-deploy.sh
+++ debinux-deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install i3 lightdm lightdm-gtk-greeter pcmanfm inkscape audacity gimp neofetch htop firefox
+apt-get install i3 lightdm lightdm-gtk-greeter pcmanfm inkscape audacity gimp neofetch htop firefox feh
+mkdir Pictures
+mkdir ~/.config/i3/
+mv index.jpeg ~/Pictures
 mv config ~/.config/i3/
 systemctl reboot
