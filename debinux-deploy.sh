0ec6ac515e27e3780affaa153b71f05a2fc9e784
YourPalTaika <94190326+YourPalTaika@users.noreply.github.com>
2021-11-12 20:18:11 +0000

    Update debinux-deploy.sh

--- debinux-deploy.sh
+++ debinux-deploy.sh
@@ -3,6 +3,8 @@ sudo apt-get update
 sudo apt-get install i3 lightdm lightdm-gtk-greeter pcmanfm inkscape audacity gimp neofetch htop firefox-esr feh terminator pulseaudio pavucontrol geany vim
 mkdir ~/Pictures
 mkdir ~/.config/i3/
+mkdir ~/.config/gtk-3.0/
+cp settings.ini ~/.configure/gtk-3.0/settings.ini
 cp debinux.png ~/Pictures
 sudo cp config /etc/i3/config
 cp i3NOTICE-READ ~/
