884554b17a8f083070c95fa6cfe77246c01cb20e
YourPalTaika <94190326+YourPalTaika@users.noreply.github.com>
2021-11-14 07:28:24 -0500

    Update debinux-amd-proprietary.sh

--- debinux-amd-proprietary.sh
+++ debinux-amd-proprietary.sh
@@ -1,1 +1,11 @@
-apt install firmware-linux firmware-linux-nonfree libdrm-amdgpu1 xserver-xorg-video-amdgpu
+#!/usr/bin/env bash
+
+###################### NOTICE - PLEASE READ ######################
+#                                                                #
+#          ONLY RUN IF YOU NEED AMD PROPRIETARY DRIVERS          #
+#                                                                #
+##################################################################
+
+sudo apt update
+sudo apt install firmware-linux firmware-linux-nonfree libdrm-amdgpu1 xserver-xorg-video-amdgpu
+echo 'OK'
