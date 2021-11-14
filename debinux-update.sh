81e324d763f5158a6ed0493382cedb562ec22822
YourPalTaika <94190326+YourPalTaika@users.noreply.github.com>
2021-11-14 09:07:45 -0500

    Update debinux-update.sh

--- debinux-update.sh
+++ debinux-update.sh
@@ -1,1 +1,6 @@
+#!/usr/bin/env bash
 
+wget https://github.com/YourPalTaika/Debinux/blob/main/config
+cp config ~/.config/i3/config
+
+echo 'All should be good, now hit win+shift+c so i3 reloads its config (its instant)'
