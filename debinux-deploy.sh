c838f9725055680c462f8e948840761bf7c26c8b
YourPalTaika <94190326+YourPalTaika@users.noreply.github.com>
2021-11-14 16:16:55 -0500

    Update debinux-deploy.sh

--- debinux-deploy.sh
+++ debinux-deploy.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install i3 lightdm lightdm-gtk-greeter pcmanfm inkscape audacity gimp neofetch htop firefox-esr feh terminator pulseaudio pavucontrol geany vim lxappearance wpagui flatpak
+sudo apt-get install i3 lightdm lightdm-gtk-greeter pcmanfm inkscape audacity gimp neofetch htop firefox-esr feh terminator pulseaudio pavucontrol geany vim lxappearance wpagui flatpak picom
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install kdenlive
 mkdir ~/.config
 mkdir ~/.config/i3/
 mkdir ~/.config/gtk-3.0/
+mkdir ~/.config/picom/
 mkdir ~/Pictures
 cp settings.ini ~/.config/gtk-3.0/settings.ini
 cp .gtkrc-2.0 ~/
+cp picom.conf ~/.config/pciom/picom.conf
 cp config ~/.config/i3/config
 cp debinux.png ~/Pictures
 cp .bashrc ~/
