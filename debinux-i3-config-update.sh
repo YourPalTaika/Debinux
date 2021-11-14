dcd6a00b1581f6dcde40b0278309c1b2ebffa99d
YourPalTaika <94190326+YourPalTaika@users.noreply.github.com>
2021-11-14 10:12:28 -0500

    Update and rename debinux-update.sh to debinux-i3-config-update.sh

---
+++ debinux-i3-config-update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -rf Debinux
+git clone https://github.com/YourPalTaika/Debinux
+cp ~/Debinux/config ~/.config/i3/config
+
+echo "i3 Configuration Updated and restored, lease press win+shift+c and i3 will reload it's config"
+echo "Note this only updates i3's custom config"
+echo 'OK'
--- debinux-update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://github.com/YourPalTaika/Debinux/blob/main/config
-cp config ~/.config/i3/config
-
-echo 'All should be good, now hit win+shift+c so i3 reloads its config (its instant)'
